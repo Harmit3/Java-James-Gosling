2135e2917529b551623431dbac76a5e58bf96c46
Harmit Patel <harmitpatel110@gmail.com>
2025-08-05 23:20:38 -0400

    Array started.

---
+++ SumitSir/V92/Main/src/Main/Main.java
@@ -0,0 +1,15 @@
+package Main;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        //arrays
+        int[] marks=new int[100];
+        marks[0]=98;
+        marks[1]=99;
+        marks[2]=99;
+        System.out.println(marks[0]+" "+marks[1]+" "+marks[2]);
+    }
+}
